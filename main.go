24892636a25ee7119c7c23698076a35a6cd89a7f
mot <mot.contact.address.pc@gmail.com>
2015-02-06 00:05:58 +0900

    :sushi:

--- main.go
+++ main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/ChimeraCoder/anaconda"
 	"github.com/otiai10/twistream"
 	"io/ioutil"
+	"regexp"
 )
 
 type Config struct {
@@ -22,20 +24,35 @@ func main() {
 	var config Config
 	json.Unmarshal(file, &config)
 
-	CONSUMERKEY := config.ConsumerKey
-	CONSUMERSECRET := config.ConsumerSecret
-	ACCESSTOKEN := config.AccessToken
-	ACCESSTOKENSECRET := config.AccessTokenSecret
+	consumerKey := config.ConsumerKey
+	consumerSecret := config.ConsumerSecret
+	accessToken := config.AccessToken
+	accessTokenSecret := config.AccessTokenSecret
 	timeline, _ := twistream.New(
 		"https://userstream.twitter.com/1.1/user.json",
-		CONSUMERKEY,
-		CONSUMERSECRET,
-		ACCESSTOKEN,
-		ACCESSTOKENSECRET,
+		consumerKey,
+		consumerSecret,
+		accessToken,
+		accessTokenSecret,
 	)
+
+	anaconda.SetConsumerKey(consumerKey)
+	anaconda.SetConsumerSecret(consumerSecret)
+	api := anaconda.NewTwitterApi(accessToken, accessTokenSecret)
 
 	for {
 		status := <-timeline.Listen()
-		fmt.Println(status)
+
+		if IsContainSushi(status.Text) {
+			api.Favorite(status.Id)
+		}
+		fmt.Println(status.Text)
 	}
+}
+
+func IsContainSushi(text string) (b bool) {
+	if m, _ := regexp.MatchString("すし|寿司|スシ|鮨|寿し|🍣|sushi", text); !m {
+		return false
+	}
+	return true
 }
