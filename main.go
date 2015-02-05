c96091936c361263d51ed6d19b6581cf014e9da1
mot <mot.contact.address.pc@gmail.com>
2015-02-06 00:21:25 +0900

    ひらがなの「すし」がかなり迷惑かけそうなので直す

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func main() {
 }
 
 func IsContainSushi(text string) (b bool) {
-	if m, _ := regexp.MatchString("すし|寿司|スシ|鮨|寿し|🍣|sushi", text); !m {
+	if m, _ := regexp.MatchString("寿司|スシ|鮨|寿し|🍣|sushi", text); !m {
 		return false
 	}
 	return true
